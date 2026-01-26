b14a74ca0af17e1b2617228bf9c471806ef9f8d1
ProRobloxPlayer2 <wangziyi6518@gmail.com>
2026-01-26 18:29:16 +0800

    Delete Rankblox-web directory

--- Rankblox-web/games/clicker/game.js
+++
@@ -1,9 +0,0 @@
-let score = 0;
-
-const scoreText = document.getElementById("score");
-const button = document.getElementById("clickBtn");
-
-button.addEventListener("click", () => {
-  score++;
-  scoreText.textContent = score;
-});
